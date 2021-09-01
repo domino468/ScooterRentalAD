bd53dc15e939a14b21cfea7e45277097fd97e25a
domino468 <dominik.adam4538@gmail.com>
2021-09-01 17:18:11 +0200

    End of basic CRUD of Rental Scooters

--- src/main/java/rental/RentalService.java
+++ src/main/java/rental/RentalService.java
@@ -2,10 +2,8 @@ package rental;
 
 import com.scooterrental.webapp.scooter.Scooter;
 import com.scooterrental.webapp.scooter.ScooterService;
-import com.scooterrental.webapp.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Optional;
 

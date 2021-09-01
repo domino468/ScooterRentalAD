9fb4f47917c0232b3d0a53c2e6219e9c1824a318
domino468 <dominik.adam4538@gmail.com>
2021-09-01 17:35:10 +0200

    Adding UserController

---
+++ src/main/java/com/scooterrental/webapp/Station/SelectedStationBean.java
@@ -0,0 +1,17 @@
+package com.scooterrental.webapp.Station;
+
+public class SelectedStationBean {
+
+    private Station station;
+
+    public SelectedStationBean() {
+    }
+
+    public Station getStation() {
+        return station;
+    }
+
+    public void setStation(Station station) {
+        this.station = station;
+    }
+}
